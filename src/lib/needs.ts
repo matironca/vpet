a7dd08f75aa287e1cc28db563ed7d8a06852eff7
matironca <86140427+matironca@users.noreply.github.com>
2024-09-15 02:38:16 -0300

    Add adjustState function.

--- src/lib/needs.ts
+++ src/lib/needs.ts
@@ -1,1 +1,12 @@
 //Functions to manage vpet needs.
+function adjustState() : void {
+    // Check and make that hunger is between the limits.
+    this.state.hunger = Math.min(this.stateMaxs.hunger, Math.max(this.stateMins.hunger, this.state.hunger));
+    
+    // Check and make that happiness is between limits.
+    this.state.happiness = Math.min(this.stateMaxs.happiness, Math.max(this.stateMins.happiness, this.state.happiness));
+    
+    // Check and make that energy is between limits
+    this.state.energy = Math.min(this.stateMaxs.energy, Math.max(this.stateMins.energy, this.state.energy));
+  }
+  
