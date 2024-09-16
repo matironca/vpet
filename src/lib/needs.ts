aac862192b8eb31c50bb5c678f964e1a4daa729a
matironca <86140427+matironca@users.noreply.github.com>
2024-09-16 10:28:00 -0300

    Make feed and sleep functions, comment out adjust state function.

--- src/lib/needs.ts
+++ src/lib/needs.ts
@@ -1,4 +1,10 @@
-//Functions to manage vpet needs.
+import * as vpet from '../lib/vpet'
+export enum food{
+    Meat = 50,
+    Carrot = 25,
+    Chocolate = 75
+}
+/*Functions to manage vpet needs.
 function adjustState() : void {
     // Check and make that hunger is between the limits.
     this.state.hunger = Math.min(this.stateMaxs.hunger, Math.max(this.stateMins.hunger, this.state.hunger));
@@ -9,4 +15,19 @@ function adjustState() : void {
     // Check and make that energy is between limits
     this.state.energy = Math.min(this.stateMaxs.energy, Math.max(this.stateMins.energy, this.state.energy));
   }
+  */
+  function feed(pet : vpet.Vpet , food : food) : void {
+//Adds the food value to the vpet hunger.
+    pet.state.hunger += food ;
+    if (pet.state.hunger > pet.stateMaxs.hunger){
+        pet.state.hunger = pet.stateMaxs.hunger;
+    }
+  }
   
+  function sleep(pet : vpet.Vpet) :void {
+//Adds 100 to the vpet energy.
+    pet.state.energy += 100;
+    if(pet.state.energy > pet.stateMaxs.energy){
+        pet.state.energy = pet.stateMaxs.energy;
+    }
+  }
