fbe1b306fb5c3c1fb6d1f4eb41dee659317dca8b
matironca <86140427+matironca@users.noreply.github.com>
2024-09-21 12:05:03 -0300

    Make .gitignore file and ignore .js files.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as vpet from './lib/vpet'
 import * as needs from './lib/needs'
-function start(petName : string) : void{
-    var pet : vpet.Vpet = new vpet.Vpet(petName, new vpet.StateLimit() , new vpet.StateLimit); 
-    var exit = 0;
-    while(exit == 0){
-        //console.log(vpet.printState);
+    let petName;
+    petName = "pato";
+    let pet : vpet.Vpet = new vpet.Vpet(petName, new vpet.StateLimit() , new vpet.StateLimit); 
+    let exit = false;
+    while(!exit){
+        console.log(pet.name);
+        exit = true;
     }
-}
