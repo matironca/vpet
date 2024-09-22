2516b229111ee701478f35397cf9c875a8a9196c
matironca <86140427+matironca@users.noreply.github.com>
2024-09-22 11:47:59 -0300

    Start menu function (Work in Progress).

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import * as vpet from './lib/vpet'
 import * as needs from './lib/needs'
+
+const readline = require('readline'); //Definition of the user interface
+const rl = readline.createInterface({
+    input: process.stdin,  // stdin
+    output: process.stdout // stdout
+  });
+
+function menu(){
+    console.log('1. Show state');
+    rl.question('Choose operation: ', (op : number) => {
+        
+        switch(op){
+            case 1:
+                vpet.showState(pet);
+                break;
+        }
+        
+        rl.close(); // Close the readline interface
+      });
+
+}
+
+//Main code
     let petName;
     petName = "pato";
     let pet : vpet.Vpet = new vpet.Vpet(petName, new vpet.StateLimit() , new vpet.StateLimit); 
     let exit = false;
     while(!exit){
         console.log(pet.name);
+        vpet.logBar(600, 1000);
+        menu();
         exit = true;
     }
