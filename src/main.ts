1a9c68476d2ffafdf2918626ffaffc5d2b7eef0e
matironca <86140427+matironca@users.noreply.github.com>
2024-10-06 13:23:41 -0300

    Add function for sleep button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { feed, passTime , food  } from './lib/needs.js';
+import { feed, passTime , food, sleep , sleepV2  } from './lib/needs.js';
 import {Vpet , showState , StateLimit} from './lib/vpet.js'
 import { wait } from './lib/tools.js';
+
 //Functions
 async function updateState(pet:Vpet , divText : HTMLElement | null) {
 //Updates the state bars
@@ -26,16 +27,39 @@ function menu(){
 }
 //Buttons Code
 window.onload = function(){
-    const feedButton = document.getElementById("FEED_PET");
+    //Feed button
+    const feedButton = document.getElementById("FEED_PET") as HTMLButtonElement;
     if(feedButton){
         feedButton.addEventListener('click', () => feed(pet , food.Carrot));
     }
+    //Sleep button
+    const sleepButton = document.getElementById("SLEEP_PET") as HTMLButtonElement;
+    if(sleepButton){
+        sleepButton.addEventListener('click', async () => {
+            sleepButton.disabled = true;
+            feedButton.disabled = true;
+            sleeping = true;
+            await sleepV2(pet);
+            sleeping = false;
+            sleepButton.disabled = false;
+            feedButton.disabled = false;
+
+        } )
+    }
 }
+
 //Main code
     let petCreated : boolean = false;
     let pet : Vpet | null = createPet();
     petCreated = true; 
+    let sleeping : boolean = false;
+
     if(petCreated = true){
+        if (sleeping = false){
+            passTime(pet, 1,0,1);
+        }
+        else{
         passTime(pet, 1,1,1);
+        }
         updateState(pet , null);
     }
